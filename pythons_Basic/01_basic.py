c691e16372671dc4bc276996a73e9046206fb8c3
Lakhya2503 <25032002shindelaxman@gmail.com>
2026-08-05 20:01:32 +0530

    add initial content for 01_basic.py

---
+++ pythons_Basic/01_basic.py
@@ -0,0 +1,14 @@
+# FastApi
+
+# what we learn first on start the course 
+# data types
+# loops
+# functions
+# decorators
+# virtual ENv
+
+
+#  data types in python
+# int, str, number
+# list, dict, set, tuple, class, object
+# 
