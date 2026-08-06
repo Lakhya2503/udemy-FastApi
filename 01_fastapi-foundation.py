99ea99ff38f0adadf48f6845b967e1210e4cfa79
Lakhya2503 <25032002shindelaxman@gmail.com>
2026-08-06 21:20:47 +0530

    add initial FastAPI application with root and about endpoints

---
+++ 01_fastapi-foundation.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+from fastapi import Request
+import uvicorn
+
+app = FastAPI(
+    title="Zomato Order Service",
+    description=(
+        "Internal API for managing Orders",
+        "It will handle creation, Traking of delivery system."
+    ),
+    version="1.1.2",
+    docs_url="/docs",
+    redoc_url="/redoc",
+    openapi_url="/openapi.json"
+)
+
+@app.get("/")
+def read_root():
+    """"Root endpoint - Health Check"""
+    # FastAPI Convertes this dic to json
+    return { "message" : "Welcome to Zomato Order Service", "status": "Healthy" }
+
+@app.get("/about")
+def about():
+    """"Returns API metadata"""
+    return { 
+        "service " : "Order service",
+        "team" : "backend Platform",
+        "region" : "ap-south-1",
+        "version" : "1.1.2"
+    }
